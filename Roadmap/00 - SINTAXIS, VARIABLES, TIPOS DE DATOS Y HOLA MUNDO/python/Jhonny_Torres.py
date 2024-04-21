d92a60489ec250d869cb390a059158362e614611
Jhonny_Torres <128723713+Saber235@users.noreply.github.com>
2024-04-21 00:13:39 -0500

    Jhonny_Torres.py
    00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jhonny_Torres.py
@@ -0,0 +1,58 @@
+"""
+Created on Sat Apr 20 22:23:08 2024
+
+@author: Jhonny Torres alias Jhonky"__________"
+"""
+
+## Este es el sitio web oficial de Python: https://www.python.org
+
+#Este es un comentario en una sola linea "____"
+
+#Comentario 
+#Multilíneas
+#Sigan 
+#Viendo"_______"
+
+"""
+Este es un comentario
+escrito en
+más de una línea
+"""
+
+'''
+Este es un comentario
+escrito en
+más de una línea
+'''
+
+Edad = 19
+Nombres = "Jhonny Alexander"
+Apellidos = "Gonzalez Torres"
+Altura = 1.81
+Peso = 75.3
+Programador_Jr = True
+Futbolista_Profesional = False
+
+"""
+Ejemplo de creación de una constante
+
+@Alert : Ojo en si las constantes en este lenguage no existen simplemente son convenciones
+Que pueden ser modificadas"_______"
+"""
+
+PI = 3.14159
+
+#Tipos de datos"____"
+print(type(Edad))
+print(type(Nombres))
+print(type(Apellidos))
+print(type(Altura))
+print(type(Peso))
+print(type(Programador_Jr))
+print(type(Futbolista_Profesional))
+
+
+print(PI)  # Salida: 3.14159
+
+print(Nombres,Apellidos)
+print("¡Hola, Python!")
